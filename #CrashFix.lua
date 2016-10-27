b0c55a542f3e01ff98000464cc83718992164b5e
D3ftsu <sahraouisharp@gmail.com>
2016-10-27 22:47:39 +0100

    Update #CrashFix.lua

--- #CrashFix.lua
+++ #CrashFix.lua
@@ -1,80 +1,38 @@
-function GetWebResultAsync(url, callback)
+function GetWebResultAsync(url, callback, UseHttps)
+  local UseHttps = UseHttps or true
   url = url:gsub("https://", ""):gsub("http://", "")
+  local file = ""
   local GotResult = false
-  local started = false
-  local result = ""
   local socket = require("socket").tcp()
+  socket:connect('gamingonsteroids.com', 80)
+  socket:send("GET "..'/GOS/TCPUpdater/GetScript'..(UseHttps and '5' or '6')..'.php?script='..Base64Encode(url)..'&rand='..math.random(99999999).." HTTP/1.1\r\nHost: gamingonsteroids.com\r\n\r\n")
   socket:settimeout(0, 'b')
   socket:settimeout(99999999, 't')
-  socket:connect('gamingonsteroids.com', 80)
   Callback.Add("Tick", function()
     if GotResult then return end
     s, status, partial = socket:receive(1024)
-    if status == 'timeout' and not started then
-      started = true
-      socket:send("GET "..'/GOS/TCPUpdater/GetScript5.php?script='..Base64Encode(url)..'&rand='..math.random(99999999).." HTTP/1.1\r\nHost: gamingonsteroids.com\r\n\r\n")
-    end
-    result = result .. (s or partial)
-    if result:find('</scr'..'ipt>') then
-      result = result:sub(result:find('\r\n\r\n'),-1)
-      local i, ContentStart = result:find('<scr'..'ipt>')
-      local ContentEnd = result:find('</scr'..'ipt>')
-      if not ContentStart or not ContentEnd then return end
+    file = file .. (s or partial)
+    if file:find('</scr'..'ipt>') then
       GotResult = true
       socket:close()
-      socket = nil
+      file = file:sub(file:find('\r\n\r\n'),-1)
+      local i, ContentStart = file:find('<scr'..'ipt>')
+      local ContentEnd = file:find('</scr'..'ipt>')
+      if not ContentStart or not ContentEnd then return end
       if callback and type(callback) == 'function' then
-        callback(Base64Decode(result:sub(ContentStart + 1, ContentEnd - 1)))
+        callback(Base64Decode(file:sub(ContentStart + 1, ContentEnd - 1)))
       end
     end
   end)
 end
 
 function DownloadFileAsync(url, path, callback)
-  url = url:gsub("https://", ""):gsub("http://", "")
-  local filesize
-  local GotFile = false
-  local started = false
-  local result = ""
-  local socket = require("socket").tcp()
-  socket:settimeout(0, 'b')
-  socket:settimeout(99999999, 't')
-  socket:connect('gamingonsteroids.com', 80)
-  Callback.Add("Tick", function()
-    if GotFile then return end
-    s, status, partial = socket:receive(1024)
-    if status == 'timeout' and not started then
-      started = true
-      socket:send("GET "..'/GOS/TCPUpdater/GetScript5.php?script='..Base64Encode(url)..'&rand='..math.random(99999999).." HTTP/1.1\r\nHost: gamingonsteroids.com\r\n\r\n")
-    end
-    result = result .. (s or partial)
-    if result:find('</si'..'ze>') then
-      if not filesize then
-        filesize = tonumber(result:sub(result:find('<si'..'ze>')+6,result:find('</si'..'ze>')-1))
-      end
-    end
-    if result:find('</scr'..'ipt>') then
-      result = result:sub(result:find('\r\n\r\n'),-1)
-      local file = ""
-      for line,content in ipairs(result:split('\n')) do
-        if content:len() > 5 then
-          file = file..content
-        end
-      end
-      local i, ContentStart = file:find('<scr'..'ipt>')
-      local ContentEnd = file:find('</scr'..'ipt>')
-      if not ContentStart or not ContentEnd then return end
-      local newf = file:sub(ContentStart+1,ContentEnd-1):gsub('\r','')
-      if newf:len() ~= filesize then return end
-      local f = io.open(path,"w+b")
-      f:write(Base64Decode(newf))
-      f:close()
-      GotFile = true
-      socket:close()
-      socket = nil
-      if callback and type(callback) == 'function' then
-        callback()
-      end
+  GetWebResultAsync(url, function(data) 
+    local f = io.open(path,"w+b")
+    f:write(data)
+    f:close()
+    if callback and type(callback) == 'function' then 
+      callback() 
     end
   end)
 end
