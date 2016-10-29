5cf1c5f2a490c86ff0b06218f61a3fda030ed909
D3ftsu <sahraouisharp@gmail.com>
2016-10-30 00:59:27 +0100

    Update #CrashFix.lua

--- #CrashFix.lua
+++ #CrashFix.lua
@@ -1,33 +1,3 @@
-local function Base64Encode(data)
-  local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-  return ((data:gsub('.', function(x)
-    local r,b='',x:byte()
-    for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
-    return r;
-  end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
-    if (#x < 6) then return '' end
-    local c=0
-    for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
-    return b:sub(c+1,c+1)
-  end)..({ '', '==', '=' })[#data%3+1])
-end
-
-local function Base64Decode(data) -- fere too op
-  local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-  data = string.gsub(data, '[^'..b..'=]', '')
-  return (data:gsub('.', function(x)
-    if (x == '=') then return '' end
-    local r,f='',(b:find(x)-1)
-    for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
-    return r;
-  end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-    if (#x ~= 8) then return '' end
-    local c=0
-    for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
-    return string.char(c)
-  end))
-end
-
 function GetWebResultAsync(url, callback, UseHttps)
   local UseHttps = UseHttps or true
   local url = url:gsub("https://", ""):gsub("http://", "")
@@ -45,7 +15,8 @@ function GetWebResultAsync(url, callback, UseHttps)
     if file:find('</scr'..'ipt>') then
       GotResult = true
       socket:close()
-      file = file:sub(file:find('\r\n\r\n'),-1)
+      local a, b = file:find('\r\n\r\n')
+      file = file:sub(a,-1)
       local i, ContentStart = file:find('<scr'..'ipt>')
       local ContentEnd = file:find('</scr'..'ipt>')
       if not ContentStart or not ContentEnd then return end
